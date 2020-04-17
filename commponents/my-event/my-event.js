bc0febca8a72ef68204a100430a0d3548cabbd89
无人山谷 <943027274@qq.com>
2020-04-17 15:08:37 +0800

    自定义组件

---
+++ commponents/my-event/my-event.js
@@ -0,0 +1,27 @@
+// commponents/my-event/my-event.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleIncrement() {
+      // console.log('---------')
+      this.triggerEvent('increment', 
+        {name: '老八', age: 18}, {})
+    }
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -5,9 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    counter: 0
   },
-
+  handleIncrement(event){
+    // console.log('----', event)
+    this.setData({
+      counter: this.data.counter+1
+    })
+  },
+  handleTabClick(event){
+    console.log(event)
+  },
+  handleSel(){
+    // 1.获取组件对象
+    const my_sel = this.selectComponent('#sel-id')
+    console.log(my_sel)
+    // 2.通过setData修改组件中的数据(不合理)
+    // my_sel.setData({
+    //   counter: my_sel.data.counter+1
+    // })
+    // 通过方法对数据进行修改
+    my_sel.incrementCounter()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
