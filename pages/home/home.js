b6263253e7624507d51bb6cb1a317678b65afdd2
无人山谷 <943027274@qq.com>
2020-04-18 16:13:02 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/home/home.js
+import request from '../../service/network'
 Page({
 
   /**
@@ -12,6 +13,44 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 使用封装的request发送网络请求
+    request({
+      url: 'http://123.207.32.32:8000/api/h8/recommend',
+    }).then(res => {
+      console.log(res)
+    })
+
+    // 发送网络请求
+    // 1.发送最简单的get请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/api/h8/recommend',
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
+    // 2.发送携带参数的get请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/api/h8/home/data',
+    //   data: {
+    //     type: 'sell',
+    //     page: 1
+    //   },
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
+    // 3.发送post请求，并且携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: 'post',
+    //   data: {
+    //     name: 'boke',
+    //     age: 11
+    //   },
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
 
   },
 
